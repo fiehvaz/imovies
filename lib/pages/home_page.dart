958af902d98a0eb6e662f2726b35e8839670ff6b
fiehvaz <vaz.louren@gmail.com>
2021-10-26 02:53:26 -0300

    added layout cards

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'package:imovies/models/movies_model.dart';
 import 'package:imovies/repositories/movies_repository.dart';
 import 'package:imovies/repositories/movies_repository_imp.dart';
 import 'package:imovies/services/dio_service_imp.dart';
+import 'package:imovies/utils/apis.utils.dart';
+import 'package:imovies/widgets/custom_list_card_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -22,16 +24,37 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ValueListenableBuilder<Movies?>(
-            valueListenable: _controller.movies,
-            builder: (_, movies, __) {
-              return movies != null
-                  ? ListView.builder(
-                      itemCount: movies.listMovies.length,
-                      itemBuilder: (_, idx) =>
-                          Text(movies.listMovies[idx].title),
-                    )
-                  : Container();
-            }));
+      body: Padding(
+        padding: const EdgeInsets.all(28),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 40),
+              Text(
+                "Movies",
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              ValueListenableBuilder<Movies?>(
+                valueListenable: _controller.movies,
+                builder: (_, movies, __) {
+                  return movies != null
+                      ? ListView.separated(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: movies.listMovies.length,
+                          itemBuilder: (_, idx) => CustomListCardWidget(
+                            movie: movies.listMovies[idx],
+                          ),
+                          separatorBuilder: (_, __) => Divider(),
+                        )
+                      : Container();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
